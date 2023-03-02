1d7968fc478d8740cdd5fa746ca3f3d922a9d010
HantianLin <1018753839wl@gmail.com>
2023-03-01 16:10:32 -0800

    fix

--- customdialog.js
+++ customdialog.js
@@ -1,4 +1,5 @@
 const alertBtn = document.getElementById('alertBtn');
+const alertDialog = document.getElementById('alertDialog');
 
 alertBtn.addEventListener('click', () => {
     alertDialog.showModal();
