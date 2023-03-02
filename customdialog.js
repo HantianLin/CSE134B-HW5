85ecf93ee1f5f0b6e3e7a6ae68e4384145ecd9fa
HantianLin <1018753839wl@gmail.com>
2023-03-01 16:44:46 -0800

    name input

--- customdialog.js
+++ customdialog.js
@@ -3,6 +3,9 @@ const alertBtn = document.getElementById('alertBtn');
 const alertDialog = document.getElementById('alertDialog');
 const confirmBtn = document.getElementById('confirmBtn');
 const confirmDialog = document.getElementById('confirmDialog');
+const promptBtn = document.getElementById('promptBtn');
+const promptDialog = document.getElementById('promptDialog');
+const nameInput = document.getElementById('name');
 
 
 alertBtn.addEventListener('click', () => {
@@ -15,4 +18,17 @@ confirmBtn.addEventListener('click', () => {
 
 confirmDialog.addEventListener('close', () => {
     outputBox.value = `Confirm result: ${confirmDialog.returnValue}`
+});
+
+promptBtn.addEventListener('click', () => {
+    promptDialog.showModal();
+});
+
+promptDialog.addEventListener('submit', () => {
+    if(promptDialog.returnValue == cancel) {
+        outputBox.value =  `Confirm result: User didn't enter anything`
+    } else {
+        const name = nameInput.value;
+        outputBox.value = `Confirm result: ${name}`
+    }
 });
