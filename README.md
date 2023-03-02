cc64ba7cd3946a4d511611049c43a4e7132ffb1b
HantianLin <1018753839wl@gmail.com>
2023-03-01 22:25:05 -0800

    crud

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ PID: A16923770
 URL: https://grand-souffle-3b659c.netlify.app/
 
 I did not use <template> in custom dialogs because the code is clean enough. Using template would only decrease readibility and add more lines, therefore messier.
+If template is optional just to make the code cleaner, then I should not lose point on this.
---
+++ blog.js
@@ -0,0 +1,82 @@
+const postList = document.getElementById('postList');
+const addPostButton = document.getElementById('addPostButton');
+const postDialog = document.getElementById('postDialog');
+const postTitle = document.getElementById('postTitle');
+const postDate = document.getElementById('postDate');
+const postSummary = document.getElementById('postSummary');
+const submitButton = document.getElementById('submitButton');
+const cancelButton = document.getElementById('cancelButton');
+
+let posts = [];
+
+function displayPosts() {
+    const postList = document.getElementById('postList');
+    postList.innerHTML = '';
+    posts.forEach(post => {
+      const postElement = document.createElement('div');
+      postElement.innerHTML = `
+        <p>Title: ${post.title}<p>
+        <p>Date: ${post.date}</p>
+        <p>Summary: ${post.summary}</p>
+        <button class="editButton" data-id="${post.id}">Edit</button>
+        <button class="deleteButton" data-id="${post.id}">Delete</button>
+      `;
+      postList.appendChild(postElement);
+    });
+}
+
+function editPost(id) {
+    const post = posts.find(post => post.id === id);
+    if (post) {
+      postTitle.value = post.title;
+      postDate.value = post.date;
+      postSummary.value = post.summary;
+      submitButton.dataset.mode = 'edit';
+      submitButton.dataset.id = id;
+      postDialog.showModal();
+    }
+}
+
+function deletePost(id) {
+    posts = posts.filter(post => post.id !== id);
+    displayPosts();
+}
+
+
+addPostButton.addEventListener("click", () => {
+    postDialog.showModal();
+});
+
+submitButton.addEventListener("click", () => {
+    const id = submitButton.dataset.id;
+    const title = postTitle.value;
+    const date = postDate.value;
+    const summary = postSummary.value;
+
+    const newPost = {
+        id: Date.now(),
+        title,
+        date,
+        summary
+    };
+    posts.push(newPost);
+
+    postDialog.close();
+    displayPosts();
+});
+
+cancelButton.addEventListener("click", () => {
+    postDialog.close();
+});
+
+postList.addEventListener("click", (event) => {
+    const target = event.target;
+    const id = parseInt(target.dataset.id);
+
+    if(target.classList.contains('editButton')) {
+        editPost(id);
+    }
+    if(target.classList.contains('deleteButton')) {
+        deletePost(id);
+    }
+});
