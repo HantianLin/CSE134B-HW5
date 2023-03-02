8dc5966aeeac569b1dba962e07c4c72911b7c95a
HantianLin <1018753839wl@gmail.com>
2023-03-02 01:53:16 -0800

    update

--- styledblog.js
+++ styledblog.js
@@ -45,8 +45,8 @@ function displayPosts() {
         <p>Title: ${post.title}<p>
         <p>Date: ${post.date}</p>
         <p>Summary: ${post.summary}</p>
-        <button class="editButton" data-id="${post.id}">Edit</button>
-        <button class="deleteButton" data-id="${post.id}">Delete</button>
+        <button class="editButton" data-id="${post.id}"><i class="fas fa-pencil-alt"></i>Edit</button>
+        <button class="deleteButton" data-id="${post.id}"><i class="fas fa-pencil-alt"></i>Delete</button>
         <hr>
       `;
       postList.appendChild(postElement);
