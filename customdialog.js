c849c3aa637cd831c842dbbe0810a60cba037141
HantianLin <1018753839wl@gmail.com>
2023-03-01 16:18:52 -0800

    confirm

--- customdialog.js
+++ customdialog.js
@@ -1,6 +1,18 @@
+const outputBox = document.querySelector('output');
 const alertBtn = document.getElementById('alertBtn');
 const alertDialog = document.getElementById('alertDialog');
+const confirmBtn = document.getElementById('confirmBtn');
+const confirmDialog = document.getElementById('confirmDialog');
+
 
 alertBtn.addEventListener('click', () => {
     alertDialog.showModal();
+});
+
+confirmBtn.addEventListener('click', () => {
+    confirmDialog.showModal();
+});
+
+confirmDialog.addEventListener('close', () => {
+    outputBox.value = `Confirm result: ${confirmDialog.returnValue}`
 });
