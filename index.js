da802482399370ee606a5e8f606007dc0db9d15e
HantianLin <1018753839wl@gmail.com>
2023-03-02 22:35:43 -0800

    button

---
+++ index.js
@@ -0,0 +1,18 @@
+const canineBtn = document.getElementById('canineBtn');
+const canineDialog = document.getElementById('canineDialog');
+const felineBtn = document.getElementById('felineBtn');
+const felineDialog = document.getElementById('felineDialog');
+const otterBtn = document.getElementById('otterBtn');
+const otterDialog = document.getElementById('otterDialog');
+
+canineBtn.addEventListener('click', () => {
+    canineDialog.showModal();
+});
+
+felineBtn.addEventListener('click', () => {
+    felineDialog.showModal();
+});
+
+otterBtn.addEventListener('click', () => {
+    otterDialog.showModal();
+});
