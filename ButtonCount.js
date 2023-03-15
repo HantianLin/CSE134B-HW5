9497d49c55fd17df8bac9d13e35e78b709eb5193
HantianLin <1018753839wl@gmail.com>
2023-03-15 16:05:37 -0700

    Update ButtonCount.js

--- ButtonCount.js
+++ ButtonCount.js
@@ -5,25 +5,19 @@ class ButtonCount extends HTMLElement {
       this.attachShadow({ mode: 'open' });
   
       const button = document.createElement('button');
-      button.textContent = 'Click me';
       button.addEventListener('click', () => {
         this.count++;
         this.render();
       });
   
       this.count = 0;
-      this.countDisplay = document.createTextNode(`Clicked ${this.count} times`);
+      this.render();
   
-      this.shadowRoot.appendChild(button);
-      this.shadowRoot.appendChild(this.countDisplay);
+      this.shadowRoot.appendChild(this.button);
     }
   
     render() {
-      this.countDisplay.textContent = `Clicked ${this.count} times`;
-    }
-  
-    connectedCallback() {
-      this.render();
+        this.button.textContent = `Clicked ${this.count} times`;
     }
   }
   
